c3d7ad308bba51efa7d6f8ec84a0cf267fe10106
pmk-sketch <pmk20021012@gmail.com>
2026-01-15 09:34:56 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 깃허브 저장소 이름인 /chat/을 기본 경로로 설정합니다.
+  base: '/chat/', 
 })
